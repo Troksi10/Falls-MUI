1b0ca18be0a502736a238dff19967391ac583b8a
lLija <ilija.trokovski@gmail.com>
2023-03-29 19:18:50 +0200

    Custom themes with providers

--- mui-project/src/components/TourCard.jsx
+++ mui-project/src/components/TourCard.jsx
@@ -5,59 +5,83 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { AccessTime } from "@mui/icons-material";
 import Rating from "@mui/material/Rating";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-const TourCard = () => {
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
+
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={4}>
-      <Paper elevation={3}>
-        <img
-          src="https://cdn.britannica.com/30/94430-050-D0FC51CD/Niagara-Falls.jpg"
-          alt=""
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img src={tour.image} alt="" className="img" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              {tour.name}
             </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.duration}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              marginTop={3}
+            >
+              <Rating
+                name="read-only"
+                value={tour.rating}
+                readOnly
+                precision={0.5}
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.rating}
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                {tour.numberOfReviews}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C ${tour.price}
+              </Typography>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-            marginTop={3}
-          >
-            <Rating
-              name="read-only"
-              value={4.5}
-              readOnly
-              precision={0.5}
-              size="small"
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={1.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From C $156
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
